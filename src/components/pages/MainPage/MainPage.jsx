6a1f158ded8cf2c979c9664a1e2cf5baf20d4a1a
SanjarKeySkills <forest2004@mail.ru>
2024-11-26 17:42:14 +0600

    26/11/24 | Slider partners adaptive

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import Header from "../../widgets/HeaderBlock/Header.jsx";
 import Hero from "../../widgets/MainBlock/Hero/Hero.jsx";
 import WelcomeBlock from "../../widgets/WelcomeBlock/WelcomeBlock.jsx";
 import InfoTableBlock from "../../widgets/InfoTableBlock/InfoTableBlock.jsx";
-// import Partners from "../../widgets/partners/Partners.jsx";
+import Partners from "../../widgets/partners/Partners.jsx";
 // import Footer from "../../widgets/FooterBlock/Footer.jsx";
 
 const MainPage = () => {
@@ -13,8 +13,8 @@ const MainPage = () => {
             <Hero />
             <WelcomeBlock />
             <InfoTableBlock />
-            {/* <Partners />
-            <Footer /> */}
+            <Partners />
+            {/* <Footer /> */}
         </div>
     );
 };
