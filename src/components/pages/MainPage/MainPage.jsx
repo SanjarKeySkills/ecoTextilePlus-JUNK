fbf854936d10bed414eb74e535d0fd99698cea89
SanjarKeySkills <forest2004@mail.ru>
2024-11-26 17:35:42 +0600

    26/22/24 | List InfoTableBlock

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../widgets/HeaderBlock/Header.jsx";
 import Hero from "../../widgets/MainBlock/Hero/Hero.jsx";
 import WelcomeBlock from "../../widgets/WelcomeBlock/WelcomeBlock.jsx";
-// import InfoTableBlock from "../../widgets/InfoTableBlock/InfoTableBlock.jsx";
+import InfoTableBlock from "../../widgets/InfoTableBlock/InfoTableBlock.jsx";
 // import Partners from "../../widgets/partners/Partners.jsx";
 // import Footer from "../../widgets/FooterBlock/Footer.jsx";
 
@@ -12,8 +12,8 @@ const MainPage = () => {
             <Header />
             <Hero />
             <WelcomeBlock />
-            {/* <InfoTableBlock />
-            <Partners />
+            <InfoTableBlock />
+            {/* <Partners />
             <Footer /> */}
         </div>
     );
