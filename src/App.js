36e83e17c18f8a7ee73b881a1db61368b18363d6
SanjarKeySkills <forest2004@mail.ru>
2025-02-11 16:59:16 +0600

    11/02/25| Fixed bag of App js Layout Vs 404

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DefaultLayout, { ContentFallback } from "./layout/DefaultLayout";
 import { MainPage } from "./pages/MainPage/MainPage.async.jsx";
-// import { ErrorPage } from "./pages/ErrorPage/ErrorPage.async.jsx";
+import { ErrorPage } from "./pages/ErrorPage/ErrorPage.async.jsx";
 
 const App = () => {
     return (
@@ -25,15 +25,15 @@ const App = () => {
                             </Suspense>
                         }
                     /> */}
-                    {/* <Route
-                        path="*"
-                        element={
-                            <Suspense fallback={<div>Loading...</div>}>
-                                <ErrorPage />
-                            </Suspense>
-                        }
-                    /> */}
                 </Route>
+                <Route
+                    path="*"
+                    element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <ErrorPage />
+                        </Suspense>
+                    }
+                />
             </Routes>
         </Router>
     );
