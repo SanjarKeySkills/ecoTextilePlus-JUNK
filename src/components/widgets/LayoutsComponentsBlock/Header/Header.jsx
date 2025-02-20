b008ffb70a87699be70f851347bab16ce344fd0a
SanjarKeySkills <forest2004@mail.ru>
2025-02-20 16:14:20 +0600

    20/02/25| half way to finish responcive header

--- src/components/widgets/LayoutsComponentsBlock/Header/Header.jsx
+++ src/components/widgets/LayoutsComponentsBlock/Header/Header.jsx
@@ -1,129 +1,167 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
-// import arrowPopUp from "../../../../assets/ui/popUpBtn.svg";
 import logo from "../../../../assets/ecotextile_logo.png";
 import burgerButton from "../../../../assets/ui/burgerBtn.svg";
+import arrowPopUp from "../../../../assets/ui/arrowBtn.svg";
 
-const firstLinks = [
-    { to: "/", label: "НА ГЛАВНУЮ" },
-    { to: "/news", label: "НОВОСТИ" },
-    { to: "/articles", label: "ПУБЛИКАЦИИ" },
+const arrLinks = [
+    { to: "/", label: "ГЛАВНАЯ" },
+    { to: "/mission", label: "МИССИЯ" },
+    { to: "/team", label: "КОМАНДА" },
     { to: "/gallery", label: "ГАЛЕРЕЯ" },
+    { to: "/gallery", label: "ВАКАНСИИ" },
+    { to: "/contacts", label: "КОНТАКТЫ" },
+];
+const arrLan = [
+    { to: "/", label: "KG" },
+    { to: "/", label: "EN" },
+    { to: "/", label: "RU" },
+];
+const arrForRecycle = [
+    { to: "/", label: "СИНТЕТИКА" },
+    { to: "/", label: "ХЛОПЧАТОБУМАЖНАЯ ТКАНЬ" },
+    { to: "/", label: "ПОЛИСТЕР" },
+    { to: "/", label: "ТРИКОТАЖ" },
+];
+const arrForProduct = [
+    { to: "/", label: "УТЕПЛИТЕЛИ" },
+    { to: "/", label: "ПАНЕЛИ УТЕПЛИТЕЛИ" },
+    { to: "/", label: "СТРОИТЕЛЬНЫЕ ПЕРЧАТКИ" },
+    { to: "/", label: "ТЕХНИЧЕСКИЕ ТКАНИ" },
 ];
 
-const secondLinks = [
-    { to: "/members", label: "GOODS", className: styles.link },
-    { to: "/about", label: "О ПАЛАТЕ", className: styles.link },
-    { to: "/board", label: "ОРГАНЫ УПРАВЛЕНИЯ", className: styles.linkTop },
-    { to: "/services", label: "УСЛУГИ", className: styles.link },
-    { to: "/contact", label: "КОНТАКТЫ", className: styles.link },
-];
+const HeaderMobileMenu = (props) => {
+    const { isOpenPopUp, togglePopup } = props;
+    const handlePopUpLinkClick = () => togglePopup(); // Закрытие адаптивного popUp меню
+    return (
+        <div
+            className={styles.containerPopUp}
+            style={{ display: isOpenPopUp ? "block" : "none" }}>
+            <div className={styles.wrapperPopUp}>
+                <div className={styles.arrowWrapper}>
+                    <img src={arrowPopUp} alt="arrow" onClick={togglePopup} />
+                </div>
+                <ul className={styles.listTopPopUp}>
+                    {arrLinks.map((link) => (
+                        <li key={link.to}>
+                            <Link
+                                to={link.to}
+                                onClick={handlePopUpLinkClick}
+                                className={styles.link}>
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={styles.wrapperBottomPopUp}>
+                <ul className={styles.listBottomPopUp}>
+                    {arrLan.map((link) => (
+                        <li key={link.to}>
+                            <Link to={link.to} onClick={handlePopUpLinkClick}>
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 const Header = () => {
     const [isOpenPopUp, setOpenPopUp] = useState(false);
-    const [isPndMenuOpen, setPndMenuOpen] = useState(false);
-    const togglePndMenu = () => setPndMenuOpen(!isPndMenuOpen);
     const togglePopup = () => setOpenPopUp(!isOpenPopUp);
-    // Функция для закрытия меню при клике на ссылку
-    const handleLinkClick = () => {
-        setPndMenuOpen(false); // Закрывает выпадающее меню "О ПАЛАТЕ"
-    };
+    const [isMenuOpen, setMenuOpen] = useState(false);
     return (
         <div className={styles.header}>
-            <div className={styles.headerContainer}>
-                <div className={styles.wrapperHeaderDesktop}>
+            <div className={styles.headerContaner}>
+                <div className={styles.headerUp}>
                     <Link to="/">
-                        <img src={logo} alt="main" />
+                        <img
+                            src={logo}
+                            alt="main"
+                            className={styles.logoHeader}
+                        />
                     </Link>
-                    <div className={styles.wrapperNavbar}>
-                        <li className={styles.dropdown}>
-                            <Link
-                                className={styles.dropdownButton}
-                                onClick={togglePndMenu}>
-                                О ПАЛАТЕ
-                            </Link>
-                            <div className={styles.dropdownContent}>
-                                <Link
-                                    on
-                                    to="/services"
-                                    className={styles.navLink}>
-                                    УСЛУГИ
-                                </Link>
-                                <Link to="/news" className={styles.navLink}>
-                                    НОВОСТИ
-                                </Link>
-                                <Link to="/articles" className={styles.navLink}>
-                                    ПУБЛИКАЦИИ
-                                </Link>
-                                <Link to="/gallery" className={styles.navLink}>
-                                    ГАЛЕРЕЯ
+                    <div className={styles.listNavbarWrapper}>
+                        <ul className={styles.listNavbar}>
+                            {arrLinks.map((link) => (
+                                <li key={link.to}>
+                                    <Link to={link.to} className={styles.link}>
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.lans}>
+                        {arrLan.map((link) => (
+                            <div key={link.to}>
+                                <Link to={link.to} className={styles.lan}>
+                                    {link.label}
                                 </Link>
                             </div>
-                        </li>
-                        <ul className={styles.listBottom}>
-                            <li>
-                                <Link
-                                    to="/about"
-                                    onClick={handleLinkClick}
-                                    className={styles.navLink}>
-                                    МИССИЯ
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/members"
-                                    onClick={handleLinkClick}
-                                    className={styles.navLink}>
-                                    НАШИ ЭКСПЕРТЫ
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/board"
-                                    onClick={handleLinkClick}
-                                    className={styles.navLink}>
-                                    ОРГАНЫ УПРАВЛЕНИЯ
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/courses"
-                                    onClick={handleLinkClick}
-                                    className={styles.navLink}>
-                                    КУРСЫ И ТРЕНИНГИ
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/contact"
-                                    onClick={handleLinkClick}
-                                    className={styles.navLink}>
-                                    КОНТАКТЫ
-                                </Link>
-                            </li>
-                        </ul>
-                        <Link
-                            onClick={handleLinkClick}
-                            className={styles.heroButton}
-                            to="/contact">
-                            ВСТУПИТЬ В ПАЛАТУ
+                        ))}
+                    </div>
+                </div>
+                <div className={styles.headerBottom}>
+                    <div className={styles.dropdown}>
+                        <Link className={styles.dropdownButton}>
+                            МЫ ПЕРЕРАБАТЫВАЕМ
                         </Link>
-                        <div className={styles.lanBtnWraper}>
-                            <Link to="/" className={styles.lan}>
-                                RU
-                            </Link>
-                            <Link to="#" className={styles.lan}>
-                                EN
-                            </Link>
-                            <Link to="#" className={styles.lan}>
-                                KG
-                            </Link>
+
+                        <div className={styles.dropdownContent}>
+                            {arrForRecycle.map((link) => (
+                                <Link
+                                    key={link.to}
+                                    className={styles.dropdownLink}>
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={styles.dropdown}>
+                        <Link className={styles.dropdownButton}>
+                            МЫ ПРОИЗВОДИМ
+                        </Link>
+
+                        <div className={styles.dropdownContent}>
+                            {arrForProduct.map((link) => (
+                                <Link
+                                    key={link.to}
+                                    className={styles.dropdownLink}>
+                                    {link.label}
+                                </Link>
+                            ))}
                         </div>
                     </div>
                 </div>
+                <div className={styles.contacts}>
+                    <p>
+                        Кыргызская Республика <br />
+                        г.Бишкек ул. Ахунбаева 169 <br />7 этаж, кабинет 1
+                    </p>
+                    <p>
+                        +996 552 702 740 <tr />
+                    </p>
+                    <a href="/">askatmazhitov@gmail.com</a>
+                    <a href="/contact" className={styles.contactBtn}>
+                        СВЯЗАТЬСЯ С НАМИ
+                    </a>
+                </div>
             </div>
-
+            <div className={styles.headerMobile}>
+                <Link to="/">
+                    <img src={logo} alt="main" />
+                </Link>
+                <img src={burgerButton} alt="burger" onClick={togglePopup} />
+                <HeaderMobileMenu
+                    isOpenPopUp={isOpenPopUp}
+                    togglePopup={togglePopup}
+                />
+            </div>
             <div className={styles.headerMobile}>
                 <Link to="/">
                     <img src={logo} alt="main" />
@@ -137,57 +175,5 @@ const Header = () => {
         </div>
     );
 };
-
-// mobile
-// const HeaderMobileMenu = (props) => {
-//     const { isOpenPopUp, togglePopup } = props;
-//     const handlePopUpLinkClick = () => togglePopup(); // Закрытие адаптивного popUp меню
-
-//     return (
-//         <div
-//             className={styles.containerPopUp}
-//             style={{ display: isOpenPopUp ? "block" : "none" }}>
-//             <div className={styles.wrapperPopUp}>
-//                 <div className={styles.arrowWrapper}>
-//                     <img src={arrowPopUp} alt="arrow" onClick={togglePopup} />
-//                 </div>
-//                 <ul className={styles.listTopPopUp}>
-//                     {firstLinks.map((link) => (
-//                         <li key={link.to}>
-//                             <Link
-//                                 to={link.to}
-//                                 onClick={handlePopUpLinkClick}
-//                                 className={styles.link}>
-//                                 {link.label}
-//                             </Link>
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//             <div className={styles.wrapperBottomPopUp}>
-//                 <ul className={styles.listBottomPopUp}>
-//                     {secondLinks.map((link) => (
-//                         <li key={link.to}>
-//                             <Link to={link.to} onClick={handlePopUpLinkClick}>
-//                                 {link.label}
-//                             </Link>
-//                         </li>
-//                     ))}
-//                 </ul>
-//                 <div className={styles.lanBtnWraper}>
-//                     <Link to="#" className={styles.lan}>
-//                         RU
-//                     </Link>
-//                     <Link to="#" className={styles.lan}>
-//                         EN
-//                     </Link>
-//                     <Link to="#" className={styles.lan}>
-//                         KG
-//                     </Link>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
 
 export default Header;
--- src/layout/DefaultLayout/index.jsx
+++ src/layout/DefaultLayout/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./index.module.scss";
-import Header from "../../components/Widgets/LayoutsComponentsBlock/Header_old/Header1.jsx";
-import Footer from "../../components/Widgets/LayoutsComponentsBlock/Footer_old/Footer1.jsx";
+import Header from "../../components/Widgets/LayoutsComponentsBlock/Header/Header.jsx";
+// import Footer from "../../components/Widgets/LayoutsComponentsBlock/Footer_old/Footer1.jsx";
 
 export const ContentFallback = () => <div className={styles.loader}></div>;
 
