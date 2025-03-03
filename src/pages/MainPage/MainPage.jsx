77a2e25b6005c6cc8975ed6620f815532ee5aa8d
SanjarKeySkills <forest2004@mail.ru>
2025-03-03 15:08:08 +0600

    03/03/25| redeployq Hero Block

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import HeroBlock from "../../components/Widgets/HeroBlock/HeroBlock.jsx";
-import MediaBlock from "../../components/Widgets/MediaBlock/MediaBlock.jsx";
+import HeroBlock from "../../components/Widgets/HeroBlock/HeroBlock";
+import MediaBlock from "../../components/Widgets/MediaBlock/MediaBlock";
 // import WelcomeBlock from "../../components/Widgets/WelcomeBlock/WelcomeBlock.jsx";
 // import InfoTableBlock from "../../components/Widgets/InfoTableBlock/InfoTableBlock.jsx";
 // import PartnersBlock from "../../components/Widgets/PartnersBlock/PartnersBlock.jsx";
