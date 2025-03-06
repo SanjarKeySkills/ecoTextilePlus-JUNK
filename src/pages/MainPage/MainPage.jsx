12a4dd97761a21a1da29e7fe0e7098004dc8e942
SanjarKeySkills <forest2004@mail.ru>
2025-03-06 14:10:25 +0600

    06/03/25| adjust HeroBlock

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import HeroBlock from "../../components/Widgets/HeroBlock/HeroBlock.jsx";
-// import MediaBlock from "../../components/Widgets/MediaBlock/MediaBlock";
-// import WelcomeBlock from "../../components/Widgets/WelcomeBlock/WelcomeBlock.jsx";
-// import InfoTableBlock from "../../components/Widgets/InfoTableBlock/InfoTableBlock.jsx";
-// import PartnersBlock from "../../components/Widgets/PartnersBlock/PartnersBlock.jsx";
-// import GalleryBlock from "../../components/Widgets/GalleryBlock/GalleryBlock.jsx";
+// import HeroBlock from "../../components/Widgets/HeroBlock/heroBlock";
+import MediaBlock from "../../components/Widgets/MediaBlock/MediaBlock";
+import WelcomeBlock from "../../components/Widgets/WelcomeBlock/WelcomeBlock.jsx";
+import InfoTableBlock from "../../components/Widgets/InfoTableBlock/InfoTableBlock.jsx";
+import PartnersBlock from "../../components/Widgets/PartnersBlock/PartnersBlock.jsx";
+import GalleryBlock from "../../components/Widgets/GalleryBlock/GalleryBlock.jsx";
 
 const MainPage = () => {
     return (
         <div>
-            <HeroBlock />
-            {/* <MediaBlock />
-            <WelcomeBlock /> */}
-            {/* <InfoTableBlock />
+            {/* <HeroBlock /> */}
+            <MediaBlock />
+            <WelcomeBlock />
+            <InfoTableBlock />
             <PartnersBlock />
-            <GalleryBlock /> */}
+            <GalleryBlock />
         </div>
     );
 };
