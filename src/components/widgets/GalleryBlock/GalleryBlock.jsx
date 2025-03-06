515915f627638c556250d783995092c280fa17ce
SanjarKeySkills <forest2004@mail.ru>
2025-03-06 16:27:10 +0600

    06/03/25| Adjust Gallery Block

--- src/components/widgets/GalleryBlock/GalleryBlock.jsx
+++ src/components/widgets/GalleryBlock/GalleryBlock.jsx
@@ -5,7 +5,7 @@ import img2 from "../../../assets/gallery/img2.png";
 import img3 from "../../../assets/gallery/img3.png";
 import img4 from "../../../assets/gallery/img4.png";
 import img5 from "../../../assets/gallery/img5.png";
-// import img6 from "../../../assets/gallery/img6.png";
+import img6 from "../../../assets/gallery/img6.png";
 
 const Gallery = () => {
     return (
@@ -17,6 +17,7 @@ const Gallery = () => {
                     <img src={img3} alt="img3" />
                     <img src={img4} alt="img4" />
                     <img src={img5} alt="img5" />
+                    <img src={img6} alt="img6" />
                 </div>
             </div>
         </>
